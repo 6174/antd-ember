131ce324b06d7ef9bf65a8a9c47e29ae1daaf6e9
chenxuejia <chenxuejia@idcos.com>
2016-02-20 10:07:33 +0800

    fix ember jquery bug, fire dummy project

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -16,6 +16,16 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self' 'unsafe-inline' 'unsafe-eval' use.typekit.net connect.facebook.net maps.googleapis.com maps.gstatic.com",
+      'font-src': "'self' data: use.typekit.net fonts.gstatic.com",
+      'connect-src': "'self'",
+      'img-src': "'self' www.facebook.com p.typekit.net",
+      'style-src': "'self' 'unsafe-inline' use.typekit.net",
+      'frame-src': "s-static.ak.facebook.com static.ak.facebook.com www.facebook.com"
     }
   };
 
