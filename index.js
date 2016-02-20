7f4c66e5c9f12fe9332c6dcd0af427149e5485d9
chenxuejia <chenxuejia@idcos.com>
2016-02-20 10:23:55 +0800

    引用 antd design less

--- index.js
+++ index.js
@@ -2,5 +2,20 @@
 'use strict';
 
 module.exports = {
-  name: 'antd-ember'
+  name: 'antd-ember',
+  included: function(app, parentAddon) {
+        this._super.included(app);
+        app = (parentAddon || app);
+        // var bowerDirectory = 'vendor/lib';
+        // app.import(bowerDirectory + '/bootstrap/dist/js/bootstrap.min.js');
+        // app.import(bowerDirectory + '/bootstrap-validator/dist/validator.min.js');
+        // app.import(bowerDirectory + '/bootstrap-sweetalert/lib/sweet-alert.min.js');
+        // app.import(bowerDirectory + "/font-awesome/fonts/fontawesome-webfont.eot", { destDir: "fonts" });
+        // app.import(bowerDirectory + "/font-awesome/fonts/fontawesome-webfont.svg", { destDir: "fonts" });
+        // app.import(bowerDirectory + "/font-awesome/fonts/fontawesome-webfont.ttf", { destDir: "fonts" });
+        // app.import(bowerDirectory + "/font-awesome/fonts/fontawesome-webfont.woff", { destDir: "fonts" });
+        // app.import(bowerDirectory + "/font-awesome/fonts/fontawesome-webfont.woff2", { destDir: "fonts" });
+        // app.import(bowerDirectory + "/font-awesome/fonts/FontAwesome.otf", { destDir: "fonts"});
+    }
 };
+
